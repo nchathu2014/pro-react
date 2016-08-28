96772c69396904e663dc393f8028efb0242baaee
Chathuranga <UCHATNU@PEROOT.COM>
2016-08-28 22:20:42 +0530

    react without jsx

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import ReactDOM,{render} from 'react-dom';
 import App from './components/App';
 
 
+let div1 = React.createElement('h1',null,'I am h1');
+let div2 = React.createElement('h2',null,'I am h2');
+let rootDiv = React.createElement('div',null,div1,div2);
 
 render(
-    <App/>,
+    rootDiv,
     document.getElementById('app')
 );
 
