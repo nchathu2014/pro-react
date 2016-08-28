6920def1c995fd5241504e1e79f903580d509c74
Chathuranga <UCHATNU@PEROOT.COM>
2016-08-28 16:52:38 +0530

    add App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,64 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    console.log("App initialized...");
   }
 
   render() {
+    let cardsList = [
+      {
+        id: 1,
+        title: "Read the Book",
+        description: "I should **read the whole book**",
+        status: "in-progress",
+        tasks: []
+      },
+      {
+        id: 2,
+        title: "Write some code",
+        description: "Code **along with the samples** in the book",
+        status: "todo",
+        tasks: [
+          {
+            id: 1,
+            name: "ContactList Example",
+            done: true
+          },
+          {
+            id: 2,
+            name: "Kanban Example",
+            done: false
+          },
+          {
+            id: 3,
+            name: "My own experiments",
+            done: false
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: "Create Draft Statements",
+        description: "Code along with the samples in the book",
+        status: "todo",
+        tasks: [
+          {
+            id: 1,
+            name: "ContactList Example",
+            done: true
+          }
+        ]
+      },
+      {
+        id: 4,
+        title: "Read the Book",
+        description: "I should read the whole book",
+        status: "complete",
+        tasks: []
+      },
+    ];
     return (
       <div>
-        <KanbanBoard></KanbanBoard>
+        <KanbanBoard cards={cardsList}></KanbanBoard>
       </div>
     );
   }
