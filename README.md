0768e91955cd91e573219a7f363bfee4d736b201
Chathuranga <UCHATNU@PEROOT.COM>
2016-08-30 10:01:42 +0530

    Controlled Component and Un controlled Component and Inline Styling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pro-react
 Professional React Gotchas
+
+4-lesson -> Create UI using Javascript/Element Factories
+5-lesson -> Inline Styling React Components/Form components
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,87 @@
 import  React,{Component} from 'react';
 
-class App extends Component{
+class App extends Component {
 
-    constructor(props){
-        super(props);
-        console.log("App initialized...");
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchState: "React",
+      textAreaState: "Hello React",
+      selectOptionState:"default"
     }
+  }
 
-    render(){
-        return(
-            <div>
-              <h1>Hello From React</h1>
-            </div>
-        );
-    }
+  handleOnChange(event) {
+    this.setState({
+      searchState: event.target.value.substr(0, 10)
+    });
+  }
+
+  textAreaOnChange(event) {
+    this.setState({
+      textAreaState: event.target.value
+    });
+  }
+
+  selectOnChange(event){
+    this.setState({
+      selectOptionState:event.target.value
+    });
+  }
+
+  handleSubmit(event){
+    console.log(`NAME : ${event.target.name.value} EMAIL : ${event.target.email.value}`);
+  }
+
+  render() {
+
+    let divStyle = {
+      width: 500,
+      height: 300,
+      backgroundColor: '#d31145',
+      padding: 10,
+      fontSize: 50
+    };
+
+    return (
+      <div style={divStyle}>
+        <h1>Hello From React</h1>
+
+        <span>Controlled Components</span>
+        Search Term: <input type="search" value={this.state.searchState} onChange={this.handleOnChange.bind(this)}/>
+
+        <textarea value={this.state.textAreaState} onChange={this.textAreaOnChange.bind(this)}></textarea>
+
+        <select value={this.state.selectOptionState} onChange={this.selectOnChange.bind(this)}>
+          <option value="default">---Select a Type---</option>
+          <option value="Mobile">Mobile</option>
+          <option value="Work">Work</option>
+          <option value="Home">Home</option>
+        </select>
+
+        <hr/>
+
+        <p>Uncontrolled Components</p>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className="formGroup">
+            Name: <input name="name" type="text" />
+          </div>
+          <div className="formGroup">
+            E-mail: <input name="email" type="mail" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+
+
+
+
+      </div>
+
+
+
+    );
+  }
 }
 
 export default App;
